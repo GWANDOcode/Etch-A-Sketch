57e28d06db6c94500290772f2919c54f3d567535
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-01-30 23:37:56 +0100

    Add slider html and css, chaged var in script.js
    changed row and column var to a single variable that, because
    the grid will always be square so there is no need to have a
    dedicated var for each axis

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const drawingBoard = document.querySelector(".drawing_board");
 
-let rowNumb = 16;
-let columnNumb = 16;
+let pencilColor = "red";
+let gridSize = 16;
 
 //Creates the rows and columns to the drawingboard
-for (i = 0; i < columnNumb; i++) {
+for (i = 0; i < gridSize; i++) {
 
     drawingBoard.appendChild(document.createElement("div"));
     drawingBoard.lastChild.classList.add("row");
     let row = drawingBoard.lastChild;
 
-    for (j = 0; j < rowNumb; j++) {
+    for (j = 0; j < gridSize; j++) {
        row.appendChild(document.createElement("div"));
        row.lastChild.classList.add("cell");
        console.log("I AM CELL")
@@ -20,8 +20,8 @@ for (i = 0; i < columnNumb; i++) {
 };
 
 //Sizing for each pixel
-let pixelWidth = drawingBoard.offsetWidth/rowNumb;
-let pixelHeight = drawingBoard.offsetHeight/columnNumb; 
+let pixelWidth = drawingBoard.offsetWidth/gridSize;
+let pixelHeight = drawingBoard.offsetHeight/gridSize; 
 
 const cells = document.querySelectorAll(".cell");
 
@@ -39,7 +39,7 @@ cells.forEach(cell => {
 
     cell.addEventListener("mouseover", () => {
         if (mouseDown === true) {
-        cell.style.backgroundColor = "red";
+        cell.style.backgroundColor = pencilColor;
         };
     });
 
